0ccf12c080c2aa6b942bafd28fb940e34e48efdb
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-22 15:29:51 -0600

    File updated at 2/22/2025, 3:29:51 PM

--- 692-TopKFrequentWords/692-TopKFrequentWords.py
+++ 692-TopKFrequentWords/692-TopKFrequentWords.py
@@ -1,12 +1,28 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        # Step 1 : Use Hash Map to Count word frequencies
+    # Approach 1 : Using a Hash Map
+
+        # Step 1 : Count word frequencies
         # Step 2 : Sort the words first by frequency in descending order and then lexicographically
         # Step 3 : Return the first k words from sorted list
         word_count = Counter(words)
         sorted_words = sorted(word_count.keys(), key=lambda x: (-word_count[x], x))
+        # return sorted_words[:k]
 
         # Time complexity = O(N log N) => N = number of words in list, sorting
         # Space Complexity = O(N) => sorting the word frequencies and the sorted list
 
-        return sorted_words[:k]
+    # Approach 2 : Using a min Heap
+
+        # Step 1 : Count word frequencies
+        # Step 2 : Use a min heap to find the top k frequent words
+        #        : Heap elements - tuples of (-frequency, word) -> simulate max heap
+        # Step 3 : Return the first k words from heap
+        word_count = Counter(words)
+        heap = []
+        for word, count in word_count.items():
+            heapq.heappush(heap, (-count, word))
+        result = []
+        for _ in range(k):
+            result.append(heapq.heappop(heap)[1])
+        return result
