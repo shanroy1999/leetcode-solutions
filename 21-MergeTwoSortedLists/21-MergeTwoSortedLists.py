753969cf4b232b5d3698be0a54278ac0139bdf3d
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-22 23:47:44 -0600

    File updated at 2/22/2025, 11:47:44 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -6,18 +6,44 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         # Brute force approach
-        values = []
-        while list1:
-            values.append(list1.val)
-            list1 = list1.next
-        while list2:
-            values.append(list2.val)
-            list2 = list2.next
+        # values = []
+        # while list1:
+        #     values.append(list1.val)
+        #     list1 = list1.next
+        # while list2:
+        #     values.append(list2.val)
+        #     list2 = list2.next
         
-        values.sort()
+        # values.sort()
+        # dummy = ListNode()
+        # current = dummy
+        # for val in values:
+        #     current.next = ListNode(val)
+        #     current = current.next
+        # return dummy.next
+
+        # Time complexity = O((M+N)log(M+N))
+        # Space complexity = O(M+N) -> sorting and creating new list
+
+        # Optimized Approach
         dummy = ListNode()
         current = dummy
-        for val in values:
-            current.next = ListNode(val)
+
+        # Traverse both lists
+        while list1 and list2:
+            # Compare values at current positions and append smaller values to merged list
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
             current = current.next
+        
+        # if one list exhausted => append remaining nodes from other list
+        if list1:
+            current.next = list1
+        if list2:
+            current.next = list2
+
         return dummy.next
