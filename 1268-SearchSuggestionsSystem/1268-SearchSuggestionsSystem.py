14d88a74f1233da1e704f8d7c9626ca78d800855
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-23 01:54:21 -0600

    File updated at 2/23/2025, 1:54:21 AM

--- 1268-SearchSuggestionsSystem/1268-SearchSuggestionsSystem.py
+++ 1268-SearchSuggestionsSystem/1268-SearchSuggestionsSystem.py
@@ -1,32 +1,33 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         # Brute Force Approach
-        products.sort()
-        result = []
-        for i in range(1, len(searchWord)+1):
-            prefix = searchWord[:i]
-            suggestions = [product for product in products if product.startswith(prefix)]
-            result.append(suggestions[:3])
-        return result
+        # products.sort()
+        # result = []
+        # for i in range(1, len(searchWord)+1):
+        #     prefix = searchWord[:i]
+        #     suggestions = [product for product in products if product.startswith(prefix)]
+        #     result.append(suggestions[:3])
+        # return result
 
         # Time complexity = O(M X N) => M = length of searchword, N = length of products
         # Space complexity = O(1)
 
         # Optimized Approach
-        # products.sort()
-        # result = []
-        # start, end = 0, n-1
-        # for i, c in enumerate(searchWord):
-        #     while start <= end and (i >= len(products[start]) or products[start][i] < c):
-        #         start+=1
-        #     while start <= end and (i >= len(products[start]) or products[start][i] > c):
-        #         end-=1
+        n = len(products)
+        products.sort()
+        result = []
+        start, end = 0, n-1
+        for i, c in enumerate(searchWord):
+            while start <= end and (i >= len(products[start]) or products[start][i] < c):
+                start+=1
+            while start <= end and (i >= len(products[start]) or products[end][i] > c):
+                end-=1
             
-        #     if start <= end:
-        #         result.append(products[start:min(start+3, end+1)])
-        #     else:
-        #         end.append([])
-        # return result
+            if start <= end:
+                result.append(products[start:min(start+3, end+1)])
+            else:
+                result.append([])
+        return result
 
 
 
