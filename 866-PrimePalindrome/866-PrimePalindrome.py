c0a580e0c7ce420ddbb3e4cbae0c700a6e25ec5f
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-23 12:00:24 -0600

    File updated at 2/23/2025, 12:00:24 PM

--- 866-PrimePalindrome/866-PrimePalindrome.py
+++ 866-PrimePalindrome/866-PrimePalindrome.py
@@ -1,21 +1,45 @@
 class Solution:
-    @staticmethod
-    def is_palindrome(x):
-        return (s := str(x)) == s[::-1]
-    
-    @staticmethod
-    def is_prime(x):
-        if x < 2:
+    # Brute Force approach
+    # def isPrime(self, n):
+    #     if n<2:
+    #         return False
+    #     for i in range(2, int(n**0.5)+1):
+    #         if n%i==0:
+    #             return False
+    #     return True
+
+    # Optimized approach
+    def isPrime(self, n):
+        if n<2:
             return False
-        for i in range(2, int(x**0.5 + 1)):
-            if not x % i:
+        if n in (2, 3):
+            return True
+        if n%2 == 0 or n%3==0:
+            return False
+        # Check divisibility for numbers of the form 6k ± 1 up to sqrt(n)
+        for i in range(5, int(math.isqrt(n)) + 1, 6):
+            if n%i == 0 or n%(i+2) == 0:
                 return False
         return True
+    
+    def isPalindrome(self, n):
+        return str(n) == str(n)[::-1]
+
+    def generatePalindromes(self, n):
+        while True:
+            if self.isPalindrome(n):
+                yield n
+            n+=1
+            # Skip the range 10^7 to 10^8 as there are no prime palindromes in this range
+            if 10**7 < n < 10**8:
+              n = 10**8
 
     def primePalindrome(self, n: int) -> int:
-        while True:
-            if Solution.is_palindrome(n) and Solution.is_prime(n):
-                return n
-            n += 1
-            if 10**7 < n < 10**8: # There are no prime palindromes in this range
-                n = 10**8
+        for palindrome in self.generatePalindromes(n):
+            if self.isPrime(palindrome):
+                return palindrome
+
+    # Time Complexity = O(M sqrt(M)) => M = range of palindromes
+    # Space Complexity = O(1) => only store the result
+
+    
