5a87c96ebe41f61da3144a0b3639134cace2ab8d
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-22 23:03:51 -0600

    File updated at 2/22/2025, 11:03:51 PM

--- 240-Searcha2DMatrixII/240-Searcha2DMatrixII.py
+++ 240-Searcha2DMatrixII/240-Searcha2DMatrixII.py
@@ -1,8 +1,31 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         # Brute force approach
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if matrix[i][j] == target:
-                    return True
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix[i])):
+        #         if matrix[i][j] == target:
+        #             return True
+        # return False
+        # Time complexity = O(M X N) => M = no of rows, N = no of columns
+
+        # Optimized Approach
+        # Start from top right corner of the matrix
+        # if current element == target => return True
+        # if current element > target => move left
+        # if current element < target => move down
+        # Time complexity = O(M + N) => M = no of rows, N = no of columns
+        if not matrix or not matrix[0]:
+            return False
+        
+        rows, cols = len(matrix), len(matrix[0])
+        row, col = 0, cols-1
+        
+        while row<rows and col>=0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                col -= 1                        # Move left
+            else:
+                row += 1                        # Move down
+        
         return False
