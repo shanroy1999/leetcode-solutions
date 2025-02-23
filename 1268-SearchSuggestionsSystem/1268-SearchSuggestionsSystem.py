54432a32a041ad3768e2bdcad21fe5a522908675
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-23 01:51:39 -0600

    File updated at 2/23/2025, 1:51:39 AM

--- 1268-SearchSuggestionsSystem/1268-SearchSuggestionsSystem.py
+++ 1268-SearchSuggestionsSystem/1268-SearchSuggestionsSystem.py
@@ -12,6 +12,22 @@ class Solution:
         # Time complexity = O(M X N) => M = length of searchword, N = length of products
         # Space complexity = O(1)
 
+        # Optimized Approach
+        products.sort()
+        result = []
+        start, end = 0, n-1
+        for i, c in enumerate(searchWord):
+            while start <= end and (i >= len(products[start]) or products[start][i] < c):
+                start+=1
+            while start <= end and (i >= len(products[start]) or products[start][i] > c):
+                end-=1
+            
+            if start <= end:
+                result.append(products[start:min(start+3, end+1)])
+            else:
+                end.append([])
+        return result
+
 
 
 
