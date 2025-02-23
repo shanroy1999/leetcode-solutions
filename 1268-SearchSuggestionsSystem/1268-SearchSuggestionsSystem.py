678bb68dbfe0d68bd109d076d86731f3a4ce8d7f
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-23 01:41:14 -0600

    Create file

---
+++ 1268-SearchSuggestionsSystem/1268-SearchSuggestionsSystem.py
@@ -0,0 +1,11 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        # Brute Force Approach
+        products.sort()
+        result = []
+        for i in range(1, len(searchWord)+1):
+            prefix = searchWord[:i]
+            suggestions = [product for product in products if product.startswith(prefix)]
+            result.append(suggestions[:3])
+        return result
+
