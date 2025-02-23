4d3cd45ada2ac67f74dfdee648620a8303e7b5f0
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-23 00:03:32 -0600

    File updated at 2/23/2025, 12:03:31 AM

--- 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.py
+++ 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.py
@@ -1,7 +1,8 @@
-class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # Brute Force Approach
-        def distance(point):
-            return point[0]**2 + point[1]**2
-        sorted_points = sorted(points, key=distance)
-        return sorted_points[:k]
+    heap = []
+    for point in points:
+        dist = point[0] * point[0] + point[1] * point[1]
+        heapq.heappush(heap, (-dist, point))
+        if len(heap) > K:
+            heapq.heappop(heap)
+    
+    return [tuple[1] for tuple in heap]
