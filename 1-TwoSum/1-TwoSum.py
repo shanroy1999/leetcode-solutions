333107909ec612e2a074dacc00db5271dc429a8f
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-25 00:01:29 -0600

    File updated at 2/25/2025, 12:01:29 AM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,16 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # Brute Force approach
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        # return []
+
+        # Time Complexity = O(N * N)
+        # Space Complexity = O(1)
+
+        # Optimized Approach
+        hashmap = {}
+        for i, num in enumerate(nums):
+            if num in hashmap:
+                return [hashmap[num], i]
+            hashmap[target - num] = i
         return []
 
-        # hashmap = {}
-        # for i, num in enumerate(nums):
-        #     diff = target - num
-        #     if diff in hashmap:
-        #         return [hashmap[num], i]
-        #     hashmap[diff] = i
-        # return []
+        # Time Complexity = O(N)
+        # Space Complexity = O(N)
