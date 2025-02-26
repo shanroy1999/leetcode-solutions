8b7fc4c33af86f186ea4d4ef05bc79c97611d680
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-25 19:40:43 -0600

    Update 997-FindtheTownJudge.py

--- 997-FindtheTownJudge/997-FindtheTownJudge.py
+++ 997-FindtheTownJudge/997-FindtheTownJudge.py
@@ -1,13 +1,11 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        trusted = [0] * (n + 1)
-
+        trusted = [0]*(n+1)         # Store the number of trusts of each person (people who trust the particular person)
         for a, b in trust:
-            trusted[a] -= 1
-            trusted[b] += 1
-        
-        for i in range(1, n + 1):
-            if trusted[i] == n - 1:
-                return i
-        
+            trusted[a] -= 1         # Decrement for the trustee
+            trusted[b] += 1         # Increment for trusted
+        for i in range(1, n+1):
+            if trusted[i] == n-1:       # Town judge will always get (n-1) trusts
+                return i                # Return the index of town judge
+        # No judge found
         return -1
