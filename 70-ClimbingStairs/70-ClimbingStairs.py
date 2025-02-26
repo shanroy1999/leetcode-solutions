0217af25134467f0697408d5bfef0bcf266871c8
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-25 22:16:32 -0600

    Update 70-ClimbingStairs.py

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,14 +1,37 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 3: return n
+        # n = 1 => 1 step
+        # n = 2 => (1 + 1)
+        #       => 2
+        # n = 3 => (1 + 1 + 1)
+        #       => (1 + 2)
+        #       => (2 + 1)
+        # n = 4 => (1 + 1 + 1 + 1)
+        #       => (1 + 2 + 1)
+        #       => (1 + 1 + 2)
+        #       => (2 + 1 + 1)
+        #       => (2 + 2)
+        # n = 5 => (1 + 1 + 1 + 1 + 1)
+        #       => (2 + 1 + 1 + 1)
+        #       => (1 + 2 + 1 + 1)
+        #       => (1 + 1 + 2 + 1)
+        #       => (1 + 1 + 1 + 2)
+        #       => (2 + 2 + 1)
+        #       => (1 + 2 + 2)
+        #       => (2 + 1 + 2)
 
+        # count(3) = count(2) + count(1)
+        # count(4) = count(3) + count(2)
+        # count(5) = count(4) + count(3)
+
+        if n<=3:
+            return n
         prev1 = 3
         prev2 = 2
-        cur = 0
+        curr = 0
 
-        for _ in range(3, n):
-            cur = prev1 + prev2
+        for i in range(3, n):
+            curr = prev1 + prev2
             prev2 = prev1
-            prev1 = cur
-        
-        return cur
+            prev1 = curr
+        return curr
