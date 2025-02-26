bd94d6ebf2e6e1fe1d7f8dc6c5689e94df69cdf0
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-26 15:02:21 -0600

    File updated at 2/26/2025, 3:02:21 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,11 +1,14 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # Brute Force approach
-        def isPalindrome(s):
-            return s == s[::-1]
-        longest = ""
-        for i in range(len(s)):
-            for j in range(i+1, len(s)+1):
-                if isPalindrome(s[i:j]) and len(s[i:j])>len(longest):
-                    longest = s[i:j]
-        return longest
+        if len(s) <= 1:
+            return s
+        
+        Max_Len=1
+        Max_Str=s[0]
+        for i in range(len(s)-1):
+            for j in range(i+1,len(s)):
+                if j-i+1 > Max_Len and s[i:j+1] == s[i:j+1][::-1]:
+                    Max_Len = j-i+1
+                    Max_Str = s[i:j+1]
+
+        return Max_Str
