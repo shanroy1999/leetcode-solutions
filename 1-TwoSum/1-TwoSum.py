2cd368d74710092010091c8b8b4120dc64900977
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-25 18:53:46 -0600

    Update 1-TwoSum.py

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -18,5 +18,11 @@ class Solution:
             hashmap[target - num] = i
         return []
 
+        # iterate -> [(2, 0), (7, 1), (11, 2), (15, 3)]
+        # hashmap = {}
+        # hashmap[9-2] = 0 => hashmap[7] = 0
+        # hashmap = {7 : 0}
+        # 7 in hashmap => return [0, 1]
+
         # Time Complexity = O(N)
         # Space Complexity = O(N)
