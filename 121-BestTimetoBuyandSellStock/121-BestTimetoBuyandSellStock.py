bacde8f4a110ce78497d5763218c8b6a757b3eb9
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-26 01:47:16 -0600

    File updated at 2/26/2025, 1:47:16 AM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -1,27 +1,4 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        # Brute Force approach
-        # max_profit = 0
-        # for i in range(len(prices)):
-        #     for j in range(i+1, len(prices)):
-        #         profit = prices[j] - prices[i]
-        #         if profit > max_profit:
-        #             max_profit = profit
-        # return max_profit
+buy_price = 7
+profit = 0
 
-        # Time complexity = O(N) => N = number of days
-        # Space Complexity = O(1)
-
-        # Optimized approach
-        min_price = float('inf')
-        max_profit = 0
-        for price in prices:
-            if price < min_price:
-                min_price = price
-            profit = price - min_price
-            if profit > max_profit:
-                max_profit = profit
-        return max_profit
-
-        # Time Complexity = O(N) => N = number of days
-        # Space Complexity = O(1)
+7 is the first price in input array
