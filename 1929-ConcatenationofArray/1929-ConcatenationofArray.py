183ef4f58c505891b87dd0ea79e0b81d8ffa9a4b
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-25 18:50:16 -0600

    Update 1929-ConcatenationofArray.py

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.py
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -1,8 +1,22 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        result = []
-        for num in nums:
-            result.append(num)  # Add each element from nums
-        for num in nums:
-            result.append(num)  # Add each element again
+        # METHOD 1 : 
+        # return nums*2
+
+        # METHOD 2 : 
+        # return nums + nums
+
+        # METHOD 3 : 
+        # nums.extend(nums)
+        # return nums
+
+        # Time Complexity = O(N)
+        # Space Complexity = O(N)
+
+        # METHOD 4:
+        n = len(nums)
+        result = [0]*(2 * n)        # Create new list of size 2n
+        for i in range(n):
+            result[i] = nums[i]
+            result[i + n] = nums[i]
         return result
