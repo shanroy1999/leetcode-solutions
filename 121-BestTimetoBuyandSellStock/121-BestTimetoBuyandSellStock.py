07de336838d23c17852f22587fe7639953a3f282
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-26 01:48:41 -0600

    File updated at 2/26/2025, 1:48:41 AM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -1,4 +1,10 @@
-buy_price = 7
-profit = 0
-
-7 is the first price in input array
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        buy_price = prices[0]
+        for i in range(1, len(prices)):
+            if buy_price > prices[i]:
+                buy_price = prices[i]
+            elif prices[i] - buy_price > profit:
+                profit = prices[i] - buy_price
+        return profit
