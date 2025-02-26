61b6dcb31289a4fec8b0d03b9e79938752a7d141
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-25 21:11:08 -0600

    File updated at 2/25/2025, 9:11:07 PM

--- 27-RemoveElement/27-RemoveElement.py
+++ 27-RemoveElement/27-RemoveElement.py
@@ -1,24 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        index = 0
-        for i in range(len(nums)):
-            if(nums[i]!=val):
-                nums[index] = nums[i]
-                index+=1
-        return index
+        k = 0
 
-        # nums = [3, 2, 2, 3], val = 3
-        # index = 0 => nums[index] = 3
-        # i in range(len(nums)) => i = 0, 1, 2, 3
-        # i = 0 => nums[i] = 3
-        #       => nums[i] = val = 3
-        # i = 1 => nums[i] = 2
-        #       => nums[i] != val => nums[0] (=nums[index]) = nums[i] = 2
-        #       => index = 1
-        # i = 2 => nums[i] = 2
-        #       => nums[i] != val => nums[1] (=nums[index]) = nums[i] = 2
-        #       => index = 2
-        # i = 3 => nums[i] = 3
-        #       => nums[i] = val = 3
-        # nums => [2, 2]
-        # => return index => index = 2
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        
+        return k
