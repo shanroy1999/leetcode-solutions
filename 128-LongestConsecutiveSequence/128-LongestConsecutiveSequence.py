0bfb6a7fb4da59860fd7619aa17f9fba3e1bb6cb
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-27 13:08:51 -0600

    Update 128-LongestConsecutiveSequence.py

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.py
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.py
@@ -1,18 +1,31 @@
-# Python3
-# Brute-force Solution
 class Solution:
-    def find(self, nums, a):
-        for i in nums:
-            if i == a:
-                return True
-        return False
-
     def longestConsecutive(self, nums: List[int]) -> int:
-        if len(nums) == 0:  return 0
-        maxi = 1
-        for i in nums:
-            conseq = 1
-            while self.find(nums, i + conseq):
-                conseq += 1
-            maxi = max(maxi, conseq)
-        return maxi
+        # Brute Force Approach
+        # nums.sort()
+        # if len(nums)==0:
+        #     return 0
+        # l = 1
+        # maxl = 1
+        # for i in range(len(nums)-1):
+        #     if(nums[i+1] == nums[i]):
+        #         continue
+        #     if(nums[i+1] == nums[i] + 1):
+        #         l += 1
+        #     else:
+        #         l = 1
+        #     maxl = max(maxl, l)
+        # return maxl
+
+        # Optimized approach
+        if len(nums)==0:
+            return 0
+        numSet = set(nums)
+        longest = 0
+        for i in numSet:
+            # Check if the number is start of a sequence
+            if (i-1) not in numSet:
+                length = 1
+                while (i+length) in numSet:
+                    length+=1
+                longest = max(longest, length)
+        return longest
