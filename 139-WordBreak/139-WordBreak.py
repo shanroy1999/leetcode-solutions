e93178fca671fbb4caeb7ed4940cf5305fa59ee4
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-27 21:18:10 -0600

    File updated at 2/27/2025, 9:18:09 PM

--- 139-WordBreak/139-WordBreak.py
+++ 139-WordBreak/139-WordBreak.py
@@ -1,24 +1,11 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # Brute Force approach
-        # def canSegment(s, wordDict):
-        #     if not s:
-        #         return True
-        #     for i in range(1, len(s)+1):
-        #         prefix = s[:i]
-        #         if prefix in wordDict and canSegment(s[i:], wordDict):
-        #             return True
-        #     return False
-        # return canSegment(s, set(wordDict))
-
-        # Optimized approach
-        # Dynamic Programming
-        dp = [False] * (len(s)+1)
-        dp[0] = [True]
-        for i in range(1, len(s)+1):
+        dp = [False]*(len(s)+1)         # Base Case
+        dp[len(s)] = True
+        for i in range(len(s)-1, -1, -1):
             for w in wordDict:
-                start = i - len(w)
-                if start >=0 and dp[start] and s[start:i]==w:
-                    dp[i] = True
+                if (i+len(w)) <= len(s) and s[i:i+len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
                     break
-        return dp[-1]
+        return dp[0]
