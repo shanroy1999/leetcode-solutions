c99a25c08d0ef06ae18e38dcb3d93057aababd01
Shantanu Roy <shan.roy1999@gmail.com>
2025-02-27 18:50:21 -0600

    Update 5-LongestPalindromicSubstring.py

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,38 +1,40 @@
 class Solution:
-    def countSubstrings(self, s: str) -> int:
-        count = 0
-        # Brute force approach
-        # for i in range(len(s)):
-        #     for j in range(i, len(s)):
-        #         if s[i:j+1] == s[i:j+1][::-1]:
-        #             count+=1
-        # return count
+    def longestPalindrome(self, s: str) -> str:
+        # Brute Force Approach
+        # if len(s)<=1:
+        #     return s
+        # max_length = 1
+        # max_str = s[0]
+        # for i in range(len(s)-1):
+        #     for j in range(i+1, len(s)):
+        #         if j-i+1 > max_length and s[i:j+1] == s[i:j+1][::-1]:
+        #             max_length = j-i+1
+        #             max_str = s[i:j+1]
+        # return max_str
+
+        # Time complexity = O(N^3)
+        # Space Complexity = O(1)
+
+        # Expand from the middle
+        if not s:
+            return ""
+
+        def expandAroundCenter(str, left, right):
+            # Out of bound check
+            while left >= 0 and right < len(str) and str[left] == str[right]:
+                left -= 1
+                right += 1
+            return right-left-1
+        
+        start = 0
+        end = 0
 
         for i in range(len(s)):
-            left = right = i
-            # Check for inbound for odd length palindrome
+            odd = expandAroundCenter(s, i, i)
+            even = expandAroundCenter(s, i, i+1)
 
-            # count += self.countPalindrome(s, left, right)
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                count+=1
-                left-=1
-                right+=1
-            # different initialization of left and right pointers for even lengths palindrome
-            left = i
-            right = i+1
-
-            # count += self.countPalindrome(s, left, right)
-            # Check for inbound for even length palindrome
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                count+=1
-                left-=1
-                right+=1
-        return count
-
-    # def countPalindrome(self, str, l, r):
-    #     res = 0
-    #     while l >= 0 and r < len(str) and str[l] == str[r]:
-    #         res+=1
-    #         l-=1
-    #         r+=1
-    #     return res
+            max_length = max(odd, even)
+            if max_length > end-start:
+                start = i - (max_length-1)//2
+                end = i + max_length//2
+        return s[start:end+1]
