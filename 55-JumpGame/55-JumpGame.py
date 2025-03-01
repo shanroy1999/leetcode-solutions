ce83edd6db18f02fd03102339b9839b8661025f9
Shantanu Roy <shan.roy1999@gmail.com>
2025-03-01 16:14:21 -0600

    Create file

---
+++ 55-JumpGame/55-JumpGame.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # Greedy Approach
+        # Trying to reach the last index of input array
+        goal = len(nums)-1
+
+        # Start from the last index
+        for i in range(len(nums)-1, -1, -1):
+            # Shift the goal post if we are able to reach the goal from the adjacent index val
+            if nums[i] + i >= goal:
+                goal = i
+        
+        # If the goal reached the first index => return True => able to jump to the goal
+        return goal == 0
