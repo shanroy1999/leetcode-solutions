7314570273d77e889cd0bf40f9e832242dfe87fd
Shantanu Roy <shan.roy1999@gmail.com>
2025-03-03 02:59:47 -0600

    File updated at 3/3/2025, 2:59:47 AM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,23 +1,14 @@
-# Python Solution
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # Method 1
-        hashmap = {}
+        count = {}
+        freq = [[] for _ in range(len(nums)+1)]
         for num in nums:
-            hashmap[num] = 1 + hashmap.get(num, 0)
-        l = [key for key, _ in sorted(hashmap.items(), key=lambda x: x[1], reverse=True)]
-        return l[:k]
-
-        # Method 2
-        # hashmap = {}
-        # for num in nums:
-        #     hashmap[num] = 1 + hashmap.get(num, 0)
-        # buckets = [[] for i in range(len(nums)+1)]
-        # for key, val in hashmap.items():
-        #     buckets[val].append(key)
-        # ans = []
-        # for i in range(len(buckets)-1, 0, -1):
-        #     for num in buckets[i]:
-        #         ans.append(num)
-        #         if len(ans) == k:
-        #             return ans
+            count[num] = 1 + count.get(num, 0)
+        for num, count in count.items():
+            freq[count].append(num)
+        res = []
+        for i in range(len(freq)-1, -1, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
