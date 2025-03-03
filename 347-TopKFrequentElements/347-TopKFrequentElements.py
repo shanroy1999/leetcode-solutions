d2bfaf2bcb7c7e68d3e2989f2fcaf5e5279cd340
Shantanu Roy <shan.roy1999@gmail.com>
2025-03-03 03:02:11 -0600

    File updated at 3/3/2025, 3:02:11 AM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,14 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for _ in range(len(nums)+1)]
+        d = defaultdict(int)
         for num in nums:
-            count[num] = 1 + count.get(num, 0)
-        for num, count in count.items():
-            freq[count].append(num)
-        res = []
-        for i in range(len(freq)-1, -1, -1):
-            for num in freq[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
+            d[num] += 1
+        result = sorted(d.keys(), key=lambda x: d[x], reverse=True)[:k]
+        return result
