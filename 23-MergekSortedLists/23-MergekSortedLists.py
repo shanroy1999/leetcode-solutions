19089261879b5c71cc824cd84abe3f4c720caaa3
Shantanu Roy <shan.roy1999@gmail.com>
2025-03-04 11:12:46 -0600

    File updated at 3/4/2025, 11:12:45 AM

--- 23-MergekSortedLists/23-MergekSortedLists.py
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -5,53 +5,32 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # Brute Force Approach
-        # Traverse all the linked lists and collect all values into a single list
-        # Sort the list
-        # Create new linked list from sorted list
-        # values = []
-        # for l in lists:
-        #     while l:
-        #         values.append(l.val)
-        #         l = l.next
-        
-        # values.sort()
+        if not lists or len(lists) == 0:
+            return None
+        # Take pair of linked list and keep merging them
+        # Keep reducing the lists until there is only 1 left
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1)<len(lists) else None
+                mergedLists.append(self.mergeList(l1, l2))
+            lists = mergedLists
+        return lists[0]
 
-        # dummy = ListNode()
-        # current = dummy
-        # for val in values:
-        #     current.next = ListNode(val)
-        #     current = current.next
-        
-        # return dummy.next
-
-        # Time complexity = O(N log N) => N is total number of nodes
-        # Space complexity = O(N) => storing the values and creating new linked list
-
-        # Optimized approach
-        # Push first node of each linked list in a min-heap
-        # Pop the smallest from the heap and append to merged list
-        # Push next node from same linked list into the heap
-        # Return head of the merged list
-
-        heap = []
-        for i, l in enumerate(lists):
-            if l:
-                heapq.heappush(heap, (l.val, i, l))  # Store (val, index, node)
-                # Heap => [(1, 0, 1->4->5), (1, 1, 1->3->4), [2, 2, 2->6]]
-        
+    def mergeList(self, list1, list2):
         dummy = ListNode()
-        current = dummy
-
-        while heap:
-            # Pop the smallest node
-            val, i, node = heapq.heappop(heap)
-            # Pop (1, 0, 1->4->5) from the heap => Add it to the merged list
-            current.next = node
-            current = current.next
-            # Push next node (4->5) into the heap
-            if node.next:
-                heapq.heappush(heap, (node.next.val, i, node.next))
-                # Heap => [(1, 1, 1->3->4), (2, 2, 2->6), (4, 0, 4->5)]
-        
+        curr = dummy
+        while list1 and list2:
+            if list1.val > list2.val:
+                curr.next = list2
+                list2 = list2.next
+            else:
+                curr.next = list1
+                list1 = list1.next
+            curr = curr.next
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
         return dummy.next
