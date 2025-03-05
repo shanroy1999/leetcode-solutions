f606157738994cda74fb7730f2a8e4b2ade6c59b
Shantanu Roy <shan.roy1999@gmail.com>
2025-03-05 16:48:59 -0600

    File updated at 3/5/2025, 4:48:58 PM

--- 937-ReorderDatainLogFiles/937-ReorderDatainLogFiles.py
+++ 937-ReorderDatainLogFiles/937-ReorderDatainLogFiles.py
@@ -2,15 +2,15 @@ class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
         letter_logs = []
         digit_logs = []
+
         for log in logs:
-            identifier, content = log.split(" ", 1)     # Return a list with 2 elements
-            if content[0].isdigit():
+            logSplit = log.split(" ", 1)
+            if any(char.isdigit() for char in logSplit[1]):
                 digit_logs.append(log)
             else:
                 letter_logs.append(log)
-            
+        
+        # sorted by identifier first
+        # then sorted by content
         letter_logs.sort(key=lambda x: (x.split(" ", 1)[1], x.split(" ", 1)[0]))
         return letter_logs + digit_logs
-
-        # Time complexity => O(N log N) => N = number of logs, Sorting step also there
-        # Space complexity => O(N) => list of logs
