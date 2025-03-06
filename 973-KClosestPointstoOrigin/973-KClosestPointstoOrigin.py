7e87f5ae2e5a52067f7880eb8599a117cb3cf4b2
Shantanu Roy <shan.roy1999@gmail.com>
2025-03-05 18:30:56 -0600

    File updated at 3/5/2025, 6:30:55 PM

--- 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.py
+++ 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.py
@@ -1,27 +1,13 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        def distance(point):
-            return point[0]**2 + point[1]**2
-
-        # Brute Force Approach
-        # sorted_points = sorted(points, key=distance)
-        # return sorted_points[:k]
-
-        # Time complexity = O(N log N) -> N = number of points + Sorting
-        # Space complexity = O(N)
-
-        # Optimized Approach
-        # Calculate euclidean distance for each point from origin
-        # push all points in a min-heap based on their distances
-        # pop the top K points
-
-        heap = []
-        for point in points:
-            heapq.heappush(heap, (-distance(point), point))
-            if len(heap)>k:
-                heapq.heappop(heap)
-            
-        return [point for (_, point) in heap]
-
-        # Time complexity = O(N log k) => N = number of points
-        # Space complexity = O(k) => storing the heap
+        # Brute Force => calculate the distances -> sort the points based on distance -> return
+        # time complexity = O(NlogN)
+        res = []
+        dist = []
+        for x, y in points:
+            d = x**2 + y**2
+            dist.append([d, x, y])
+        dist.sort(key=lambda x: x[0])
+        for i in dist[:k]:
+            res.append([i[1], i[2]])
+        return res
